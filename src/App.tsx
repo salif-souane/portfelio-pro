751df28d687dd9ee03cdb2311c9e94808770667c
salif-souane <salifsouane1234@gmail.com>
2025-09-14 16:01:36 +0200

    mise a jour

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
+
 import Home from "./pages/Home";
-import About from "./pages/About";
-import Projects from "./pages/Projects";
-import Contact from "./pages/Contact";
+
 
 function App() {
   return (
@@ -14,17 +11,15 @@ function App() {
 
         {/* Contenu principal */}
        
-          <Navbar />
+       
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/projects" element={<Projects />} />
-            <Route path="/contact" element={<Contact />} />
+         
+            
           </Routes>
         
 
-        {/* Footer */}
-        <Footer />
+     
       
     </Router>
   );
